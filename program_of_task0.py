ff80d3eceaa7200fd9054fcf5d1a7d24b9c73b04
Satwant Singh Hanspal <satwantsinghhanspal@TC526.WPA.Dal.Ca>
2020-02-20 14:02:31 -0400

    few changes

--- program_of_task0.py
+++ program_of_task0.py
@@ -5,9 +5,9 @@ def new_func():
             print("Fizz")
         elif i % 5 == 0:
             print("Buzz")
-        elif i % 3 ==0 and i % 5 == 0:
+        elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         else:
             print(i)
-    # print(i)
+    
 new_func() 
