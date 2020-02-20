25464821036eb3b65e5ee69bead9a283f5d70b6d
Satwant Singh Hanspal <satwantsinghhanspal@TC526.WPA.Dal.Ca>
2020-02-20 14:00:36 -0400

    program

---
+++ program_of_task0.py
@@ -0,0 +1,13 @@
+def new_func():
+    x = range(1, 101)
+    for i in x:
+        if i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 ==0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+    # print(i)
+new_func() 
