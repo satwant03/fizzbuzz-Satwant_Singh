96d3eeafe9b6cf6907919d25968b3060fbc0ba6d
Satwant Singh Hanspal <satwantsinghhanspal@Satwants-MacBook-Pro.local>
2020-02-21 00:26:47 -0400

    second  function

--- practice.py
+++ practice.py
@@ -12,3 +12,20 @@ def unique_word():
         chokas.add(data)
     print(list(chokas))
 unique_word()
+
+def count_the_article():
+    a=0
+    fin = open("Book1.txt",'r')
+    wrd = fin.read()
+    fiss = open("Book2.txt",'r')
+    lo = fiss.read()
+    mega = open("20k.txt",'r')
+    ka = mega.read()
+    ans =(wrd + lo + ka)
+    jng = ans.split()
+    for data in jng:
+        if data in ["a", "the", "at", "run", "to","and","are","or","for","an","this"]:
+            a+=1
+    return a
+
+print(count_the_article())
